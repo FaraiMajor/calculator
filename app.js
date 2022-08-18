b60cd0323f4c6b48c8c8fd868f1c95666ee80bcb
Farai Major <faraimutukumira@gmail.com>
2022-08-18 16:38:59 -0400

    calculator 90% done

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const boxText = document.querySelector('p');
-const numbers = document.querySelectorAll('.number')
-const operator = document.querySelectorAll('.operator');
-const percentage = document.querySelector('.modulo');
-const backspace = document.querySelector('.backspace');
-const deleteAll = document.querySelector('.erase');
-const equals = document.querySelector('.equals');
-
 function roundAns(n) {
     return Math.round((n + Number.EPSILON) * 1000) / 1000
 }
@@ -36,6 +28,8 @@ function modulo(x, y) {
 }
 
 function operate(operator, x, y) {
+    x = Number(x);
+    y = Number(y);
     switch (operator) {
         case '+':
             return add(x, y);
@@ -49,23 +43,76 @@ function operate(operator, x, y) {
         case '*':
             return multiply(x, y);
         case '/':
-            if (y == 0) {
-                boxText.textContent = "Error";
-            } else
-                return divide(x, y);
+            return divide(x, y);
+            break;
+        case '%':
+            return modulo(x, y);
             break;
     }
 }
 
-function updateDisplay() {
+const boxText = document.querySelector('.screen');
+const numbers = document.querySelectorAll('.number')
+const operator = document.querySelectorAll('.operator');
+const percentage = document.querySelector('.modulo');
+const backspace = document.querySelector('.backspace');
+const deleteAll = document.querySelector('.erase');
+const equals = document.querySelector('.equals');
+const keys = document.querySelector('.btnContainer')
 
+let firstOperand, secondOperand, displayValue, op, operation;
+let isOperator = false;
+let result = 0;
+
+function init() {
+    firstOperand = 0;
+    secondOperand = 0;
+    displayValue = '';
+    result = 0;
+    boxText.innerHTML = result;
 }
 
-// function roundAns(answer) {
-//     if (answer.toString().indexOf('.') !== -1) {
-//       if (answer.toString().split('.')[1].length > 5) {
-//         return answer.toFixed(5);
-//       }
-//     }
-//     return answer;
-//   }
+// runs as soon as user clicked one of the number key
+function updateDisplay(e) {
+    displayValue += e.target.textContent;
+    boxText.innerHTML = displayValue;
+    if (isOperator) {
+        secondOperand = e.target.textContent;
+        displayValue = operate(operation, firstOperand, secondOperand);
+    }
+}
+
+// runs whenever operator is clicked and save operation to global variable;
+function handleOperation(e) {
+    firstOperand = displayValue;
+    displayValue += e.target.innerHTML;
+    operation = e.target.dataset.action;
+    isOperator = true;
+    if (result) {
+        firstOperand = result;
+    }
+}
+numbers.forEach(keys =>
+    keys.addEventListener('click', e => updateDisplay(e))
+);
+operator.forEach(op =>
+    op.addEventListener('click', e => handleOperation(e))
+);
+
+//clear all
+deleteAll.addEventListener('click', e => {
+    init();
+})
+
+equals.addEventListener('click', () => {
+    // set secondOperand to be either the first number after the operation or the first number after the first operation(before the equal operator)
+    secondOperand =
+        secondOperand || displayValue.replace(firstOperand, '').match(/[^\+|\-|\*|\/]\d*/);
+    result = operate(operation, firstOperand, secondOperand);
+    boxText.innerHTML = result % 1 ? Number(result.toFixed(4)) : result;
+    operation = '';
+});
+
+
+
+init();
